26c5697ef864b5e5a9e3b60a95af3195134cd83c
Retch <retch100@gmail.com>
2021-04-27 13:49:26 +0200

    add iptables rules

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,21 @@ CONFFILE=/etc/privoxy/config
 PIDFILE=/var/run/privoxy.pid
 
 
-# wg-quick up /wireguard/wg0.conf
+# wg-quick up wg0.conf
 
-#ifname=$(basename $(ls -1 /etc/wireguard/*.conf | head -1) .conf)
-#wg-quick up /etc/wireguard/$ifname.conf
+DROUTE=$(ip route | grep default | awk '{print $3}')
+HOMENET=192.168.0.0/16
+HOMENET2=10.0.0.0/8
+HOMENET3=172.16.0.0/12
+ip route add $HOMENET3 via $DROUTE
+ip route add $HOMENET2 via $DROUTE
+ip route add $HOMENET via $DROUTE
+iptables -I OUTPUT -d $HOMENET -j ACCEPT
+iptables -A OUTPUT -d $HOMENET2 -j ACCEPT
+iptables -A OUTPUT -d $HOMENET3 -j ACCEPT
+
+ifname=$(basename $(ls -1 /etc/wireguard/*.conf | head -1) .conf)
+wg-quick up /etc/wireguard/$ifname.conf
 
 if [ ! -f "${CONFFILE}" ]; then
 	echo "Configuration file ${CONFFILE} not found!"
